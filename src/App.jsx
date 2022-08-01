1762233ca421520c05cd6c5bb69fadfaad8fbb4e
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-08-02 06:40:35 +0900

    テキストにCssModulesをあて、CSSの変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { ColoredMessage } from "./components/ColoredMessage";
+import { CssModules } from "./components/CssModules";
+
 export const App = () => {
-  return <h1>こんにちは！</h1>;
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColoredMessage />
+      <CssModules />
+    </>
+  );
 };
---
+++ src/components/ColoredMessage.jsx
@@ -0,0 +1,8 @@
+export const ColoredMessage = () => {
+  const contentStyle = {
+    color: "blue",
+    fontSize: "20pxd"
+  };
+
+  return <p style={contentStyle}>お元気ですか？</p>;
+};
