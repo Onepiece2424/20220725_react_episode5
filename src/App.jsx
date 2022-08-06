19dc199f793de2f973036a80d0ceee01720a28a2
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-08-07 06:08:05 +0900

    TailwindCssを使用したボタンの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 // /import { CssModules } from "./components/CssModules";
 // import { StyledComponents } from "./components/StyledComponents";
 // import { StyledJSX } from "./components/StyledJSX";
-import { Emotion } from "./components/Emotion";
-import { Emotion1 } from "./components/Emotion1";
-import { Emotion2 } from "./components/Emotion2";
+// import { Emotion } from "./components/Emotion";
+// import { Emotion1 } from "./components/Emotion1";
+// import { Emotion2 } from "./components/Emotion2";
+import { TailwindCss } from "./components/TailwindCss";
 
 export const App = () => {
   return (
@@ -13,9 +14,10 @@ export const App = () => {
       <CssModules /> */}
       {/* <StyledJSX /> */}
       {/* <StyledComponents /> */}
-      <Emotion />
+      {/* <Emotion />
       <Emotion1 />
-      <Emotion2 />
+      <Emotion2 /> */}
+      <TailwindCss />
     </>
   );
 };
